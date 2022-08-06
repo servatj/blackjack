2c24d551790cd67dfcccf17a79327ff7ff925983
Josep Servat <jservat@outlook.es>
2022-08-06 09:25:53 +0200

    full blackjack

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
+const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x)
+const pipe = (...fns) => x => fns.reduce((x, y) => f(y), x)
+
+
 const createDeck = () => {
   const suits = ['S', 'H', 'C', 'D']
   const values = ['A', '2', '3', '4','5','6','7','8','9', 'T', 'J', 'Q', 'K']
-  const cards = []
+  const deck = []
   suits.forEach((suit) => {
     values.forEach((value) => {
-      cards.push(`${value}${suit}`)
+      deck.push(`${value}${suit}`)
     })
   })
-  return cards;
+  return deck;
 }
 
 const shuffleDeck = (deck) => {
-  const newDeck = [...deck]
   for(let i = 0; i < deck.length; i++) {
     const numberRandom = Math.floor(Math.random() * i)
-    const temp = newDeck[i]
-    newDeck[i] = newDeck[numberRandom]
-    newDeck[numberRandom] = temp
+    const temp = deck[i]
+    deck[i] = deck[numberRandom]
+    deck[numberRandom] = temp
   }
-  return newDeck
+  return deck
+}
+
+const dealHand = (deck, n) => {
+  const hand = deck.splice(0, n)
+  return hand
+}
+
+const computeThePoints = (hand) => {
+  const values = { 'A': 1, 'T' : 10, 'J' : 10, 'Q' : 10, 'k' : 10 }
+  let handContainAces = false
+  console.log('hand', hand)
+  const value = hand.map((card) =>  {
+      if(card[0] === 'A') {
+        handContainAces = true
+      }
+      return Number(values[card[0]]) || Number(card[0])
+    }).reduce((a, b) => a + b)
+  return handContainAces && value < 10 ? value + 10 : value;
+}
+
+const game = () => {
+  const deck = compose(createDeck, shuffleDeck)([])
+  const hand = dealHand(deck, 2)
+  const hand2 = dealHand(deck, 2)
+  const p1 = computeThePoints(hand)
+  const p2 = computeThePoints(hand2)
+  console.log(p1, p2)
+  if(p1 === 21) {
+    console.log('p1 black Jack')
+  } else if (p2 == 21) {
+    console.log('p2 black Jack')
+  } else {
+    if(p1 < 21 && p2 < 21) {
+      if(p1 > p2) {
+        console.log('p1 won')
+      } else {
+        console.log('p2 won')
+      }
+    }
+  }
 }
 
 module.exports = {
   createDeck,
-  shuffleDeck
+  shuffleDeck,
+  dealHand,
+  computeThePoints,
+  game
 }
